7d3b1ce7e00d1d66a717bbce38f4169d52a7d233
wesley.anibal <wesley.anibal@virtus.ufcg.edu.br>
2018-04-30 17:32:23 -0300

    leves alterações

--- graph/src/graph/Graph.java
+++ graph/src/graph/Graph.java
@@ -2,16 +2,6 @@ package graph;
 
 import java.util.*;
 public class Graph{
-    private int V;   // No. of vertices
-    private LinkedList<Integer> adj[]; //Adjacency Lists
 
-
-    public Graph(int v){
-
-    }
-
-    public void addEdge(int v,int w) {
-        adj[v].add(w);
-    }
 }
 
